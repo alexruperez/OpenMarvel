1ae769fac72c4c8c76a5b403ffe508de5c82cc5b
Alex Rupérez <me@alexruperez.com>
2020-03-10 18:00:42 +0100

    Initial Commit

---
+++ OpenMarvelTests/OpenMarvelTests.swift
@@ -0,0 +1,34 @@
+//
+//  OpenMarvelTests.swift
+//  OpenMarvelTests
+//
+//  Created by Alex Rupérez on 10/03/2020.
+//  Copyright © 2020 alexruperez. All rights reserved.
+//
+
+import XCTest
+@testable import OpenMarvel
+
+class OpenMarvelTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
